50e7f4d1a7ed74da372b3c7af08df10912ab0bdc
Akshan Dhyani <akshandhyani987@gmail.com>
2026-01-03 21:47:17 +0530

    fix case senstivenss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import SearchBar from './componants/SearchBar'
-import UserCard from './componants/UserCard'
+import SearchBar from './Componants/SearchBar'
+import UserCard from './Componants/UserCard'
 
 function App() {
 
